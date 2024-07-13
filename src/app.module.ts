a47e1b279d4c052fc7426f479a9178a7ac5d349e
Arlan Santos <arlan05sts@gmail.com>
2024-07-13 12:28:21 -0300

    feat: add todos entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TodoModule } from './app/todo/todo.module';
 
 @Module({
   imports: [
@@ -13,12 +14,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         host: configService.get('DB_HOST', 'localhost'),
         port: Number(configService.get('DB_PORT', '5432')),
         username: configService.get('DB_USERNAME', 'root'),
-        password: configService.get('DB_PASSWORD', '123'),
-        database: configService.get('DB_DATABASE', 'todo'),
-        entities: [],
+        password: configService.get('DB_PASSWORD', '1234'),
+        database: configService.get('DB_DATABASE', 'mydb'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
         synchronize: true,
       }),
     }),
+    TodoModule,
   ],
   controllers: [],
   providers: [],
