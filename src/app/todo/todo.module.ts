ea6b0789299c87cdd1ec6877ce86eb69eb7b0624
Arlan Santos <arlan05sts@gmail.com>
2024-07-27 15:07:10 -0300

    chore: organization of imports with biomejs

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
-import { TodoService } from './todo.service';
-import { TodoController } from './todo.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TodoEntity } from './entity/todo.entity';
+import { TodoController } from './todo.controller';
+import { TodoService } from './todo.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TodoEntity])],
-  controllers: [TodoController],
-  providers: [TodoService],
-  exports: [TodoService],
+	imports: [TypeOrmModule.forFeature([TodoEntity])],
+	controllers: [TodoController],
+	providers: [TodoService],
+	exports: [TodoService],
 })
 export class TodoModule {}
