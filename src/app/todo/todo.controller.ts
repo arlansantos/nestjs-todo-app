16b3ad019ccb599e7d9527885fc2c942a72b0923
Arlan Santos <arlan05sts@gmail.com>
2024-07-27 15:06:36 -0300

    feat: add method delete

--- src/app/todo/todo.controller.ts
+++ src/app/todo/todo.controller.ts
@@ -1,40 +1,46 @@
 import {
-  Controller,
-  Post,
-  Body,
-  Get,
-  Param,
-  ParseUUIDPipe,
-  Put,
+	Body,
+	Controller,
+	Delete,
+	Get,
+	Param,
+	ParseUUIDPipe,
+	Post,
+	Put,
 } from '@nestjs/common';
-import { TodoService } from './todo.service';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
+import { TodoService } from './todo.service';
 
 @Controller('todo')
 export class TodoController {
-  constructor(private readonly todoService: TodoService) {}
+	constructor(private readonly todoService: TodoService) {}
 
-  @Post()
-  async create(@Body() createTodoDto: CreateTodoDto) {
-    return await this.todoService.create(createTodoDto);
-  }
+	@Post()
+	async create(@Body() createTodoDto: CreateTodoDto) {
+		return await this.todoService.create(createTodoDto);
+	}
 
-  @Get()
-  async index() {
-    return await this.todoService.findAll();
-  }
+	@Get()
+	async index() {
+		return await this.todoService.findAll();
+	}
 
-  @Get(':id')
-  async select(@Param('id', new ParseUUIDPipe()) id: string) {
-    return await this.todoService.findOneOrFail(id);
-  }
+	@Get(':id')
+	async select(@Param('id', new ParseUUIDPipe()) id: string) {
+		return await this.todoService.findOneOrFail(id);
+	}
 
-  @Put(':id')
-  async update(
-    @Param('id', new ParseUUIDPipe()) id: string,
-    @Body() data: UpdateTodoDto,
-  ) {
-    return await this.todoService.update(id, data);
-  }
+	@Put(':id')
+	async update(
+		@Param('id', new ParseUUIDPipe()) id: string,
+		@Body() data: UpdateTodoDto,
+	) {
+		return await this.todoService.update(id, data);
+	}
+
+	@Delete(':id')
+	async destroy(@Param('id', new ParseUUIDPipe()) id: string) {
+		await this.todoService.delete(id);
+	}
 }
--- src/app/todo/todo.service.ts
+++ src/app/todo/todo.service.ts
@@ -1,41 +1,59 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { CreateTodoDto } from './dto/create-todo.dto';
-import { Repository } from 'typeorm';
-import { TodoEntity } from './entity/todo.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
+import { TodoEntity } from './entity/todo.entity';
 
 @Injectable()
 export class TodoService {
-  constructor(
-    @InjectRepository(TodoEntity)
-    private readonly todoRepository: Repository<TodoEntity>,
-  ) {}
+	constructor(
+		@InjectRepository(TodoEntity)
+		private readonly todoRepository: Repository<TodoEntity>,
+	) {}
 
-  async create(data: CreateTodoDto): Promise<void> {
-    await this.todoRepository.save(this.todoRepository.create(data));
-  }
+	async create(data: CreateTodoDto): Promise<void> {
+		try {
+			await this.todoRepository.save(this.todoRepository.create(data));
+		} catch (error) {
+			throw new Error(error.message);
+		}
+	}
 
-  async findAll(): Promise<TodoEntity[]> {
-    return await this.todoRepository.find();
-  }
+	async findAll(): Promise<TodoEntity[]> {
+		try {
+			return await this.todoRepository.find();
+		} catch (error) {
+			throw new Error(error.message);
+		}
+	}
 
-  async findOneOrFail(id: string): Promise<TodoEntity> {
-    try {
-      return await this.todoRepository.findOneOrFail({
-        where: { id },
-      });
-    } catch (error) {
-      throw new NotFoundException(`task with id: ${id} not found`);
-    }
-  }
+	async findOneOrFail(id: string): Promise<TodoEntity> {
+		try {
+			return await this.todoRepository.findOneOrFail({
+				where: { id },
+			});
+		} catch (error) {
+			throw new NotFoundException(`task with id: ${id} not found`);
+		}
+	}
 
-  async update(id: string, data: UpdateTodoDto): Promise<void> {
-    try {
-      await this.findOneOrFail(id);
-      await this.todoRepository.update(id, data);
-    } catch (error) {
-      throw new Error(error.message);
-    }
-  }
+	async update(id: string, data: UpdateTodoDto): Promise<void> {
+		try {
+			await this.findOneOrFail(id);
+			await this.todoRepository.update(id, data);
+		} catch (error) {
+			throw new Error(error.message);
+		}
+	}
+
+	async delete(id: string): Promise<void> {
+		try {
+			await this.todoRepository.softDelete({
+				id,
+			});
+		} catch (error) {
+			throw new Error(error.message);
+		}
+	}
 }
