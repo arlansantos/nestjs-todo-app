840cf0a294f10c170fdb2d83fd21dfa0e1341647
Arlan Santos <arlan05sts@gmail.com>
2024-07-27 15:04:43 -0300

    chore: use  template literals

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,25 +4,25 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TodoModule } from './app/todo/todo.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST', 'localhost'),
-        port: Number(configService.get('DB_PORT', '5432')),
-        username: configService.get('DB_USERNAME', 'root'),
-        password: configService.get('DB_PASSWORD', '1234'),
-        database: configService.get('DB_DATABASE', 'mydb'),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: true,
-      }),
-    }),
-    TodoModule,
-  ],
-  controllers: [],
-  providers: [],
+	imports: [
+		ConfigModule.forRoot(),
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: (configService: ConfigService) => ({
+				type: 'postgres',
+				host: configService.get('DB_HOST', 'localhost'),
+				port: Number(configService.get('DB_PORT', '5432')),
+				username: configService.get('DB_USERNAME', 'root'),
+				password: configService.get('DB_PASSWORD', '1234'),
+				database: configService.get('DB_DATABASE', 'mydb'),
+				entities: [`${__dirname}/**/*.entity{.ts,.js}`],
+				synchronize: true,
+			}),
+		}),
+		TodoModule,
+	],
+	controllers: [],
+	providers: [],
 })
 export class AppModule {}
